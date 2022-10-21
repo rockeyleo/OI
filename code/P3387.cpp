627d5a9b3310764d0e2f8d25c5e99148758a7c8f
lirong <yaxiaolirong@126.com>
2022-10-21 16:58:24 +0800

    22/10/21 1659

---
+++ code/P3387.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int M = 500100;
+int head[M],nxt[M],vis[M],to[M];
+int val[M];
+int m,n;
+
+void add(int f,int t){
+    
+}
+
+int main(){
+    cin>>m>>n;
+    for(int i=1;i<=n;i++){
+        int f,t;
+        cin>>f>>t;
+        add(f,t);
+    }
+}
