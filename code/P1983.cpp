165f6133b936723199ba31fd66773cac1f3d7b57
lirong <yaxiaolirong@126.com>
2022-10-23 20:07:45 +0800

    22/10/23 2007

--- code/P1983.cpp
+++ code/P1983.cpp
@@ -1,5 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
+const int M = 1005;
+int s[M];
 int main(){
-    
+    int n,m;
+    cin>>n>>m;
+    for(int i=1;i<=m;i++){
+        int s;
+        cin>>s;
+        for(int j=1;j<=s;j++){
+            
+        }
+    }
 }
